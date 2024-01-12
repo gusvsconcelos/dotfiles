47d15e914fe7aff12405989abe7c0200b65166e5
Gustavo Vasconcelos <136681497+gusvsconcelos@users.noreply.github.com>
2024-01-12 20:46:37 -0300

    Waybar layout and Hyprland tweaks
    Changed waybar modules order, removed borders from hyprland windows, changed the color palette from cava, and some other tweaks.

--- .config/cava/scripts/update-colors.sh
+++ .config/cava/scripts/update-colors.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-color1=$( awk 'match($0, /color0=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh )
-color2=$( awk 'match($0, /color1=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh )
+color1=$( awk 'match($0, /color11=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh )
+color2=$( awk 'match($0, /color9=\47(.*)\47/,a) { print a[1] }' ~/.cache/wal/colors.sh )
 
 sed -e "s/%%%%%%%/$color1/" -e "s/+++++++/$color2/" ~/.config/cava/config.colors > /tmp/cava_colors
 pid=$(pidof cava)

689a93c13f7c5a0d62c548f6978103e33173bc27
Gustavo Vasconcelos <136681497+gusvsconcelos@users.noreply.github.com>
2024-01-27 15:04:56 -0300

    Arch Linux Hyprdots v2.1

--- .config/mako/update-colors.sh
+++ .config/mako/update-colors.sh
@@ -7,7 +7,7 @@ conffile="${HOME}/.config/mako/config"
 # Associative array, color name -> color code.
 declare -A colors
 colors=(
-    ["background-color"]="$background"bb
+    ["background-color"]="$background"cc
     ["text-color"]="$color11"
     ["border-color"]="$color13"
 )

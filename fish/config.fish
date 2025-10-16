ef756da28ab6c799b1fcaa7a152bcc74065abb8b
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2025-10-16 13:05:16 -0300

    add: hyprland setup config files

--- fish/config.fish
+++ fish/config.fish
@@ -6,11 +6,14 @@ end
 
 # Alias
 alias home='cd ~'
-alias up='fetch; echo ""; echo "ATUALIZAR SISTEMA"; yay -Syu --noconfirm'
+alias up='fastfetch; echo ""; echo "ATUALIZAR SISTEMA"; yay -Syu --noconfirm'
 alias clean='yay -Sc --noconfirm; yay -Qtdq | yay -Rns -'
 alias ls='lsd'
 alias py='python'
-alias dev='cd $HOME/Dev/'
+alias dev='cd $HOME/Documentos/Dev/'
 alias nvc='cd $HOME/.config/nvim/;nvim'
 alias nvp='cd $HOME/.config/nvim/'
 alias fetch='clear;fastfetch'
+alias m='cat /sys/class/drm/card1-HDMI-A-1/status'
+
+fish_add_path /home/gus/.spicetify

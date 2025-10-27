674257de39c52645ece821e8c32684ea4162d3c5
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2025-10-27 03:54:31 -0300

    update: make cliboard history and emoji picker menu cleaner

--- .config/rofi/scripts/clipboard.sh
+++ .config/rofi/scripts/clipboard.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cliphist list | rofi -dmenu -p "Clipboard" -theme $HOME/.config/rofi/themes/menu_no_icons.rasi | cliphist decode | wl-copy
+cliphist list | rofi -dmenu -p "Clipboard" -display-columns 2 -theme $HOME/.config/rofi/themes/menu_no_icons.rasi | cliphist decode | wl-copy

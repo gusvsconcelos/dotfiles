4a6d0c72b7cbb16461b5e82ad8d7f925b22565d9
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2025-11-05 09:53:23 -0300

    fix: fish config for ubuntu systems

---
+++ fish/config.fish
@@ -0,0 +1,18 @@
+set -g fish_greeting
+
+if status is-interactive
+    starship init fish | source
+end
+
+# ──[ Aliases ]─────────────────────────────────────────────────
+alias up='clear;fetch;echo "SYSTEM UPDATE:";yay -Syu --noconfirm'
+alias clean='yay -Scc --noconfirm; yay -Rns $(yay -Qtdq) --noconfirm'
+alias clean-full='yay -Scc --noconfirm; yay -Rns $(yay -Qtdq) --noconfirm; sudo journalctl --vacuum-size=50M --vacuum-time=2weeks; sudo rm -rf /var/tmp/* /tmp/*'
+alias ls='lsd'
+alias py='python'
+alias vim='nvim'
+alias zel='zellij'
+alias fetch='clear;fastfetch'
+alias dev='cd $HOME/Documentos/dev/'
+alias nv='cd $HOME/.config/nvim/;nvim'
+alias moviestar='clear;py $HOME/Documentos/dev/moviestar/moviestar.py'

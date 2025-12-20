2108a5725d489fe55e4a6ebf87e1a967242637e7
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2025-12-19 22:05:09 -0300

    remove fastfetch, update starship prompt config, update fish update alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,12 +5,11 @@ if status is-interactive
 end
 
 # ──[ Aliases ]─────────────────────────────────────────────────
-alias up='clear;fetch;echo "SYSTEM UPDATE:";yay -Syu --noconfirm'
+alias up='clear;fetch;echo"";echo "SYSTEM UPDATE:";yay -Syu --noconfirm'
 alias clean='$HOME/.config/fish/scripts/clean.sh'
 alias ls='lsd'
 alias py='python'
 alias vim='nvim'
 alias dev='cd $HOME/Documentos/dev/'
-alias fetch='clear;fastfetch'
 alias nv='cd $HOME/.config/nvim/;nvim'
 alias moviestar='clear;py $HOME/Documentos/dev/moviestar/moviestar.py'

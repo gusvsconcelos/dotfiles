f2be6bc7a74cb8e7f8ef3aeb38f09c5cf04f67f0
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2026-01-08 15:31:01 -0300

    feat+chore: reorganize and add new alias to new CLI Toolkit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,10 +8,19 @@ end
 # ──[ Aliases ]─────────────────────────────────────────────────
 alias up='clear;fetch;echo "SYSTEM UPDATE:";yay -Syu --noconfirm'
 alias clean='$HOME/.config/fish/scripts/clean.sh'
-alias ls='lsd'
-alias vim='nvim'
-alias py='python'
-alias ventoy='sudo $HOME/Documentos/Ventoy/VentoyWeb.sh'
 alias dev='cd $HOME/Documentos/dev/'
 alias nv='cd $HOME/.config/nvim/;nvim'
+
+# CLI Toolkit
+alias ls='eza'
+alias cat='bat'
+alias ex='yazi'
+alias zel='zellij'
+alias lgit='lazygit'
+alias ldocker='lazydocker'
+alias vim='nvim'
+
+# Misc
+alias py='python'
+alias ventoy='sudo $HOME/Documentos/Ventoy/VentoyWeb.sh'
 alias moviestar='clear;py $HOME/Documentos/dev/moviestar/moviestar.py'

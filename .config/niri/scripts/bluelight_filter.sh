d0a9227f5f73dade31aa1a90e73fcf70293fe06f
Gustavo Vasconcelos <gustavo.vs.me@gmail.com>
2026-01-30 11:26:42 -0300

    feat: make bluelight filter OSD replace the last notification instead of creating another one

--- .config/niri/scripts/bluelight_filter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if pgrep -x wlsunset > /dev/null; then
-    pkill wlsunset
-    notify-send -a bluelight_filter "Filtro de luz azul desativado"
-else
-    wlsunset -t 4000 &
-    notify-send -a bluelight_filter "Filtro de luz azul ativado"
-fi
